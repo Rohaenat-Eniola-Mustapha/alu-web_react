c4c8ac6226b98735f32082003b189d0c608546ad
rmustapha <134163158+Rohaenat-Eniola-Mustapha@users.noreply.github.com>
2025-05-08 08:45:05 +0000

    Declaration of studentsList with 2 instances done

--- TypesSript/task_0/js/main.ts
+++ TypesSript/task_0/js/main.ts
@@ -1,6 +1,28 @@
+// Student Interface Created
 interface Student {
   firstName: string;
   lastName: string;
   age: number;
   location: string;
-}
+};
+
+// Create 2 Student Variables
+
+// Student 1
+const student1: Student = {
+  firstName: "Rohaenat",
+  lastName: "Mustapha",
+  age: 19,
+  location: "Kigali"
+};
+
+// Student 2
+const student2: Student = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 21,
+  location: "Lagos"
+};
+
+// Store in array(studentList)
+const studentsList: Student[] = [student1, student2];
