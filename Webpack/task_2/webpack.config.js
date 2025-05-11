a9aa269b5152d17b33478a555b77dcc2832356f2
rmustapha <134163158+Rohaenat-Eniola-Mustapha@users.noreply.github.com>
2025-05-11 18:00:11 +0000

    Task 2 done

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192, // Inline if less than 8KB
+            },
+          },
+        ],
+      },
+    ],
+  },
+  mode: 'production',
+};
