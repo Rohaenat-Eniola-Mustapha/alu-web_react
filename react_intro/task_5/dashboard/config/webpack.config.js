e3a221888bca962a9d49caf19c785df9a4ea8078
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-05-16 07:42:58 +0000

    task 9 done, 10 start

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -19,10 +19,17 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.js$|\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'images/[name].[ext]', // Output images to an 'images' folder
+          filename: 'images/[name].[ext]',
         },
         use: [
           {
