2503c11d51e374c9a80ab009560e8de31d098f7c
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2025-05-16 07:31:06 +0000

    taskk 8 done

---
+++ react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,2 @@
+import './style.css';
+document.getElementById('root').textContent = 'Hello from Webpack with CSS';
---
+++ react_intro/task_5/dashboard/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[ext]', // Output images to an 'images' folder
+        },
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 75,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
